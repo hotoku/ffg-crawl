b1930c3fafba75d05ba4fbd379a65a510a15b5e2
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-04-03 15:47:57 +0900

    disable proxy

--- ffgcrawl/ffgcrawl/settings.py
+++ ffgcrawl/ffgcrawl/settings.py
@@ -50,10 +50,10 @@ DOWNLOAD_DELAY = 1
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-DOWNLOADER_MIDDLEWARES = {
-    'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
-    'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
-}
+# DOWNLOADER_MIDDLEWARES = {
+#     'rotating_proxies.middlewares.RotatingProxyMiddleware': 610,
+#     'rotating_proxies.middlewares.BanDetectionMiddleware': 620,
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -95,5 +95,5 @@ FEED_EXPORT_ENCODING = "utf-8"
 
 
 
-ROTATING_PROXY_LIST_PATH = 'proxy-list.txt'
-ROTATING_PROXY_PAGE_RETRY_TIMES = 5
+# ROTATING_PROXY_LIST_PATH = 'proxy-list.txt'
+# ROTATING_PROXY_PAGE_RETRY_TIMES = 5
