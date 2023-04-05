09bc9e253b981a9583619c4542c62a18b17e9ea3
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-04-05 16:48:06 +0900

    keywords

--- chat/chat/chunk.py
+++ chat/chat/chunk.py
@@ -1,4 +1,10 @@
 from collections import deque
+import re
+
+import MeCab
+import ipadic
+
+_TAGGER = MeCab.Tagger(ipadic.MECAB_ARGS)
 
 
 def split_to_chunk(s: str, max_len: int, sep: str = "。") -> list[str]:
@@ -19,4 +25,11 @@ def split_to_chunk(s: str, max_len: int, sep: str = "。") -> list[str]:
 
 
 def extract_keywords(s: str) -> list[str]:
-    pass
+    words = _TAGGER.parse(s).split("\n")
+    # 最後の2個はEOSと空文字なので削除
+    infos = list(map(lambda s: re.split(r"[\t,]", s), words))[:-2]
+    ret: list[str] = []
+    for info in infos:
+        if info[1] == "名詞" and info[2] in ("一般", "固有名詞"):
+            ret.append(info[7])
+    return ret
--- chat/tests/test_chunk.py
+++ chat/tests/test_chunk.py
@@ -1,4 +1,4 @@
-from chat.chunk import split_to_chunk
+from chat.chunk import split_to_chunk, extract_keywords
 
 
 def test_chunk():
@@ -8,36 +8,42 @@ def test_chunk():
     assert ret == ["あああ。", "いいい。", "ううう。"]
 
 
-def test_chun2():
+def test_chunk2():
     max_len = 3
     s = "あああ。いいい。ううう。"
     ret = split_to_chunk(s, max_len)
     assert ret == ["あああ。", "いいい。", "ううう。"]
 
 
-def test_chun3():
+def test_chunk3():
     max_len = 4
     s = "あああ。いいい。ううう。"
     ret = split_to_chunk(s, max_len)
     assert ret == ["あああ。いいい。", "ううう。"]
 
 
-def test_chun4():
+def test_chunk4():
     max_len = 6
     s = "あああ。いいい。ううう。"
     ret = split_to_chunk(s, max_len)
     assert ret == ["あああ。いいい。", "ううう。"]
 
 
-def test_chun5():
+def test_chunk5():
     max_len = 7
     s = "あああ。いいい。ううう。"
     ret = split_to_chunk(s, max_len)
     assert ret == ["あああ。いいい。ううう。"]
 
 
-def test_chun6():
+def test_chunk6():
     max_len = 10
     s = "あああ。いいい。ううう。"
     ret = split_to_chunk(s, max_len)
     assert ret == ["あああ。いいい。ううう。"]
+
+
+def test_keywords():
+    s = "吾輩は猫である"
+    ret = extract_keywords(s)
+    assert ret == ["猫"]
