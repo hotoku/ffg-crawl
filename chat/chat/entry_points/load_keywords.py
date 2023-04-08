1b687e23a32358b1ba0b576b4fdd84ab0d2a7edf
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2023-04-08 17:09:35 +0900

    load keywords

--- chat/chat/entry_points/load_keywords.py
+++ chat/chat/entry_points/load_keywords.py
@@ -1,6 +1,11 @@
+import logging
+
 import pandas as pd
 
 from ..db import db_con, query
+
+
+LOGGER = logging.getLogger(__name__)
 
 
 def create_keywords():
@@ -18,8 +23,11 @@ def create_keywords():
     con.commit()
 
 
-def condition(word: str, _attrs: str) -> bool:
-    attrs = eval(_attrs)
+def condition(word: str, _attrs: str | list[str]) -> bool:
+    if isinstance(_attrs, str):
+        attrs = eval(_attrs)
+    else:
+        attrs = _attrs
     return (
         word != "*" and
         attrs[0] == "名詞" and
@@ -36,6 +44,8 @@ def load_keywords():
     from
       words
 """)
+    LOGGER.info("load df. number of records = %d", len(df))
+
     flags = [
         condition(df["word"].iloc[i],
                   df["attributes"].iloc[i])
